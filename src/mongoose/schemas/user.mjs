51ca5b1df6a8a10f3075d051881b498dfb1038fd
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-02-27 08:11:48 +0300

    update

---
+++ src/mongoose/schemas/user.mjs
@@ -0,0 +1,26 @@
+import { Schema } from "mongoose";
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+   
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    created_at: Date,
+    updated_at: Date
+});
+export const user = mongoose.model('User', userSchema);
+
